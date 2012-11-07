475b1fd6b3d613e93850630228af532a6d2dd0c4
David Molineus <mail@netzmacht.de>
2012-11-07 15:42:13 +0100

    create an BackendFieldsetFilter object and fix bug with setting menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,219 @@
+function BackendFieldsetFilter()
+{
+	var self = this;
+	var widgetIdentifer = '.beFieldsetFilter';
+	var widget = $$(widgetIdentifer);
+	var searchField = $$(widgetIdentifer + ' .search');
+	var clearButton = $$(widgetIdentifer + ' .clear');
+
+	/**
+	 * enable hiding of single fields
+	 * @var bool
+	 */
+	var hideFields = true;
+
+
+	/**
+	 * set min length before search request is handled
+	 * @var int
+	 */
+	var minLength = 3;
+
+
+	/**
+	 * init BackendFieldsetFilter
+	 *
+	 * @return void
+	 */
+	self.init = function()
+	{
+		// stop form processing
+		widget.addEvent('submit', function(event) {
+			if(event) { 
+				event.stop();
+			}
+		});
+
+		// add click event to clear button
+		clearButton.addEvent('click', function(event) {
+			if(event) {
+				event.stop();
+			}
+
+			self.clear();		
+		});
+
+		// trigger clear if escape key is pressed
+		searchField.addEvent('keydown', function(event) {
+			if(event && event.key == 'esc') {
+				self.clear();
+			}
+		});
+
+		// remove classes if legend is toggled
+		$$('fieldset legend').addEvent('click', function(event) {
+			var fieldSet = this.getParent();
+			fieldSet.removeClass('beFieldsetCollapsed');
+			fieldSet.removeClass('beFieldsetOpen');
+			self.removeClassFromElements('.hidden');
+		});
+
+		// handle search requests
+		searchField.addEvent('keyup', function() {
+			self.process();
+		} );
+	}
+
+
+	/**
+	 * clear search field
+	 *
+	 * @return void
+	 */
+	self.clear = function()
+	{
+		searchField.set('value', '');
+		self.process();	
+	}
+
+
+	/**
+	 * clear search field
+	 *
+	 * @return void
+	 */
+	self.process = function()
+	{
+		var val = new String(searchField.get('value'));
+		var fieldSets = $$('.tl_formbody_edit > fieldset');
+
+		// only show clear button if search box has text
+		self.setClassConditional(clearButton, 'hidden', val.length == 0);
+
+		// clear if nothing is searched 
+		// remove all added classes
+		if(val.length == 0) {
+			fieldSets.removeClass('beFieldsetCollapsed');
+			fieldSets.removeClass('beFieldsetOpen');
+			fieldSets.each( function(fieldSet){
+				self.removeClassFromElements('hidden');
+			});
+		}
+
+		// stop processing if search string is too shoort
+		if(val.length < minLength) {
+			return;
+		}
+
+		// go through each fieldset
+		fieldSets.each(function(box) {			
+			var added = false;
+			var legend = box.getChildren('legend');
+			var showBox = false;
+
+			// lengend match so show every legend element 
+			if(legend) {
+				legend.each( function(legend) {
+					var text = legend.get('text');
+
+					if(text.test(val, 'i')) {
+						self.switchClasses(box, 'beFieldsetCollapsed', 'beFieldsetOpen');
+						self.removeClassFromElements('hidden', box);
+						showBox = true;
+					}
+				});
+			}
+
+			// check every element and hide elements if nessecary
+			if(!showBox) {
+				box.getElements('div > label, h3 > label, fieldset > legend').each( function(label) {
+			
+					var text = label.get('text');
+
+					if(text.test(val, 'i')) {
+						self.switchClasses(box, 'beFieldsetCollapsed', 'beFieldsetOpen');
+						added = true;
+
+						if( hideFields) {
+							label.getParent().getParent().removeClass('hidden');
+						}
+					}
+					else {
+						if(added == false) {
+							self.switchClasses(box, 'beFieldsetOpen', 'beFieldsetCollapsed');
+						}
+
+						if(hideFields) {
+							label.getParent().getParent().addClass('hidden');
+						}
+					}
+				});
+
+				// fire event for supporting M17StickyFooter
+				// if m17 StickyFooter isn't installed nothing should happen
+				box.fireEvent('click:relay(legend)');
+			}
+
+		});
+	}		
+
+
+	/**
+	 * remove class and add another one
+	 *
+	 * @param object obj
+	 * @param string classA
+	 * @param string classB
+	 * @return void
+	 */
+	self.switchClasses = function(obj, classA, classB) {
+		obj.removeClass(classA);
+		obj.addClass(classB);
+	}
+
+	/**
+	 * remove class from elements which has class name
+	 *
+	 * @param string className
+	 * @param optional object
+	 * @return void
+	 */
+	self.removeClassFromElements = function(className, obj)
+	{
+		if(!obj) {
+			$$('.' + className).removeClass(className);
+		}
+		else {
+			obj.getElements('.' + className).removeClass(className);
+		}
+	}
+
+	/**
+	 * add/remove class to an element depending on condition
+	 *
+	 * @param object obj
+	 * @param string className
+	 * @param bool condition
+	 * @return bool returns true if class was added
+	 */
+	self.setClassConditional = function(obj, className, condition)
+	{
+		if(condition) {
+			obj.addClass(className);
+			return true;
+		}
+
+		obj.removeClass(className);
+		return false;
+	}
+}
+
+window.addEvent('domready', function() {
+	var widget = new BackendFieldsetFilter();
+	widget.init();
+});
+
+/*
 window.addEvent('domready', function() {
 	// stop form being processed
 	$$('.beFieldsetFilter').addEvent('submit', function(event) {
@@ -113,13 +329,14 @@ window.addEvent('domready', function() {
 							label.getParent().getParent().addClass('hidden');
 						}
 					}
-
-					// fire event for supporting M17StickyFooter
-					// if m17 StickyFooter isn't installed nothing should happen
-					box.fireEvent('click:relay(legend)');
 				});
+
+				// fire event for supporting M17StickyFooter
+				// if m17 StickyFooter isn't installed nothing should happen
+				box.fireEvent('click:relay(legend)');
 			}
 
 		});
 	});
 });
+*/
