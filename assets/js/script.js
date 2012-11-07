6d7e766fd2294751bf6e6e8a7c618d18785e04f1
David Molineus <mail@netzmacht.de>
2012-11-07 11:12:18 +0100

    filter works now independent from contaos toggle status. so clear will go back to the initial state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 window.addEvent('domready', function() {
+	// stop form being processed
+	$$('.beFieldsetFilter').addEvent('submit', function(event) {
+		if(event) {
+			event.stop();
+		}
+	});
+
 	// add clear method to clear button 
 	$$('.beFieldsetFilter .clear').addEvent('click', function(event) {
 		// somehow not everytime a event object is there...		
@@ -19,12 +26,22 @@ window.addEvent('domready', function() {
 		}
 	});
 
+	// remove classes if legend is toggled
+	$$('fieldset legend').addEvent('click', function(event) {
+		var fieldSet = this.getParent();
+		fieldSet.removeClass('beFieldsetCollapsed');
+		fieldSet.removeClass('beFieldsetOpen');
+		fieldSet.getElements('.hidden').removeClass('hidden');
+	});
+
 	// filter after inserting search string
 	$$('.beFieldsetFilter .search').addEvent('keyup', function() {
 		var el = $$(this);
 		var val = new String(el.get('value'));
 		var hideFields = true;
 		var clearBtn = $$('.beFieldsetFilter .clear');
+		var minLength = 3;
+		var fieldSets = $$('.tl_formbody_edit > fieldset');
 
 
 		// only show clear button if search box has text
@@ -35,13 +52,24 @@ window.addEvent('domready', function() {
 			clearBtn.addClass('hidden');
 		}
 
+		// clear if nothing is searched 
+		// remove all added classes
+		if(val.length == 0) {
+			fieldSets.removeClass('beFieldsetCollapsed');
+			fieldSets.removeClass('beFieldsetOpen');
+			fieldSets.each( function(fieldSet){
+				fieldSet.getElements('.hidden').removeClass('hidden');
+			});
+			return;	
+		}
+
 		// set text length as min 3
-		if(val.length > 0 && val.length < 3) {
+		if(val.length < minLength) {
 			return;
 		}
 
 		// go through each fieldset
-		$$('.tl_formbody_edit > fieldset').each(function(box) {			
+		fieldSets.each(function(box) {			
 			var added = false;
 			var legend = box.getChildren('legend');
 			var showBox = false;
@@ -52,9 +80,11 @@ window.addEvent('domready', function() {
 					var text = legend.get('text');
 
 					if(text.test(val, 'i')) {
-						box.removeClass('collapsed');
-						box.getElements('.hidden').removeClass('hidden');
+						box.removeClass('beFieldsetCollapsed');
+						box.addClass('beFieldsetOpen');
 						showBox = true;
+
+						box.getElements('.hidden').removeClass('hidden');
 					}
 				});
 			}
@@ -66,7 +96,8 @@ window.addEvent('domready', function() {
 					var text = label.get('text');
 
 					if(text.test(val, 'i')) {
-						box.removeClass('collapsed');
+						box.removeClass('beFieldsetCollapsed');
+						box.addClass('beFieldsetOpen');
 						added = true;
 						if( hideFields) {
 							label.getParent().getParent().removeClass('hidden');
@@ -74,7 +105,8 @@ window.addEvent('domready', function() {
 					}
 					else {
 						if(added == false) {
-							box.addClass('collapsed');
+							box.addClass('beFieldsetCollapsed');
+							box.removeClass('beFieldsetOpen');
 						}
 
 						if(hideFields) {
