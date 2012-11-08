06e2f1eab2b10fdcfa728f22f8590724aabb7046
David Molineus <mail@netzmacht.de>
2012-11-08 08:55:19 +0100

    remove old code and fix spelling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+/** 
+ * BackendFieldsetFilter class to handle this widget in the backend
+ *
+ * @author David Molineus <mail@netzmacht.de>
+ */
 function BackendFieldsetFilter()
 {
 	var self = this;
@@ -55,7 +60,7 @@ function BackendFieldsetFilter()
 			var fieldSet = this.getParent();
 			fieldSet.removeClass('beFieldsetCollapsed');
 			fieldSet.removeClass('beFieldsetOpen');
-			self.removeClassFromElements('.hidden');
+			self.removeClassFromElements('hidden');
 		});
 
 		// handle search requests
@@ -118,7 +123,7 @@ function BackendFieldsetFilter()
 
 					if(text.test(val, 'i')) {
 						self.switchClasses(box, 'beFieldsetCollapsed', 'beFieldsetOpen');
-						self.removeClassFromElements('hidden', box);
+						self.removeClassFromElements(box, 'hidden');
 						showBox = true;
 					}
 				});
@@ -171,6 +176,7 @@ function BackendFieldsetFilter()
 		obj.addClass(classB);
 	}
 
+
 	/**
 	 * remove class from elements which has class name
 	 *
@@ -178,15 +184,16 @@ function BackendFieldsetFilter()
 	 * @param optional object
 	 * @return void
 	 */
-	self.removeClassFromElements = function(className, obj)
+	self.removeClassFromElements = function(classOrObj, className)
 	{
-		if(!obj) {
-			$$('.' + className).removeClass(className);
+		if(!className) {
+			$$('.' + classOrObj).removeClass(classOrObj);
 		}
 		else {
-			obj.getElements('.' + className).removeClass(className);
+			classOrObj.getElements('.' + className).removeClass(className);
 		}
 	}
+
 
 	/**
 	 * add/remove class to an element depending on condition
@@ -208,135 +215,9 @@ function BackendFieldsetFilter()
 	}
 }
 
+// init BackendFieldsetFilter
 window.addEvent('domready', function() {
 	var widget = new BackendFieldsetFilter();
 	widget.init();
 });
 
-/*
-window.addEvent('domready', function() {
-	// stop form being processed
-	$$('.beFieldsetFilter').addEvent('submit', function(event) {
-		if(event) {
-			event.stop();
-		}
-	});
-
-	// add clear method to clear button 
-	$$('.beFieldsetFilter .clear').addEvent('click', function(event) {
-		// somehow not everytime a event object is there...		
-		if(event) {
-			event.stop();
-		}
-		$$('.beFieldsetFilter .search').set('value', '');
-		// fire keyup event to display elements
-		$$('.beFieldsetFilter .search').fireEvent('keyup');
-	});
-
-	// trigger clear if escape key is pressed
-	$$('.beFieldsetFilter .search').addEvent('keydown', function(event) {
-		// somehow not everytime a event object is there...
-		// so keydown:keys(esc) would throw an exception sometimes
-		if(event && event.key == 'esc') {
-			$$('.beFieldsetFilter .clear').fireEvent('click');
-		}
-	});
-
-	// remove classes if legend is toggled
-	$$('fieldset legend').addEvent('click', function(event) {
-		var fieldSet = this.getParent();
-		fieldSet.removeClass('beFieldsetCollapsed');
-		fieldSet.removeClass('beFieldsetOpen');
-		fieldSet.getElements('.hidden').removeClass('hidden');
-	});
-
-	// filter after inserting search string
-	$$('.beFieldsetFilter .search').addEvent('keyup', function() {
-		var el = $$(this);
-		var val = new String(el.get('value'));
-		var hideFields = true;
-		var clearBtn = $$('.beFieldsetFilter .clear');
-		var minLength = 3;
-		var fieldSets = $$('.tl_formbody_edit > fieldset');
-
-
-		// only show clear button if search box has text
-		if(val.length > 0) {
-			clearBtn.removeClass('hidden');
-		}
-		else {
-			clearBtn.addClass('hidden');
-		}
-
-		// clear if nothing is searched 
-		// remove all added classes
-		if(val.length == 0) {
-			fieldSets.removeClass('beFieldsetCollapsed');
-			fieldSets.removeClass('beFieldsetOpen');
-			fieldSets.each( function(fieldSet){
-				fieldSet.getElements('.hidden').removeClass('hidden');
-			});
-			return;	
-		}
-
-		// set text length as min 3
-		if(val.length < minLength) {
-			return;
-		}
-
-		// go through each fieldset
-		fieldSets.each(function(box) {			
-			var added = false;
-			var legend = box.getChildren('legend');
-			var showBox = false;
-
-			// lengend match so show every legend element 
-			if(legend) {
-				legend.each( function(legend) {
-					var text = legend.get('text');
-
-					if(text.test(val, 'i')) {
-						box.removeClass('beFieldsetCollapsed');
-						box.addClass('beFieldsetOpen');
-						showBox = true;
-
-						box.getElements('.hidden').removeClass('hidden');
-					}
-				});
-			}
-
-			// check every element and hide elements if nessecary
-			if(!showBox) {
-				box.getElements('label, fieldset legend').each( function(label) {
-			
-					var text = label.get('text');
-
-					if(text.test(val, 'i')) {
-						box.removeClass('beFieldsetCollapsed');
-						box.addClass('beFieldsetOpen');
-						added = true;
-						if( hideFields) {
-							label.getParent().getParent().removeClass('hidden');
-						}
-					}
-					else {
-						if(added == false) {
-							box.addClass('beFieldsetCollapsed');
-							box.removeClass('beFieldsetOpen');
-						}
-
-						if(hideFields) {
-							label.getParent().getParent().addClass('hidden');
-						}
-					}
-				});
-
-				// fire event for supporting M17StickyFooter
-				// if m17 StickyFooter isn't installed nothing should happen
-				box.fireEvent('click:relay(legend)');
-			}
-
-		});
-	});
-});
-*/
